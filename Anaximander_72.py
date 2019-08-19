bfc7c1246eb22ec0acf15bffba2b0d0bd3ed939b
WebBreacher <WebBreacher@users.noreply.github.com>
2019-08-18 21:27:14 -0400

    adding newer var subst.; status/progress counter, more.

--- Anaximander_72.py
+++ Anaximander_72.py
@@ -14,19 +14,19 @@ import sys
 #### Functions
 def keyboardInterruptHandler(signal, frame):
     # Capture Ctrl-C and finalize the KML before we leave to capture whatever content we have in there
-    print("KeyboardInterrupt (ID: {}) has been caught. Cleaning up...".format(signal))
+    print('KeyboardInterrupt (ID: {}) has been caught. Cleaning up...'.format(signal))
     closeKml()
     exit(0)
 
 def closeKml():
     #### Finalizing and Closing KML
-    z.write("</Document>\n")
-    z.write("</kml>\n")
+    z.write('</Document>\n')
+    z.write('</kml>\n')
     z.close()
-    print("[+] Finished generating the KML file\n")
+    print('[+] Finished generating the KML file\n')
 
 #### Forces input of xml file
-parser = optparse.OptionParser('correct usage is: Anaximander.py -t' + ' CellebriteTowers.xml')
+parser = optparse.OptionParser('Correct usage is: Anaximander.py -t CellebriteTowers.xml')
 parser.add_option('-t', dest='targetpage', type='string', help='Specify the Cellebrite cellphone tower XML output file')
 (options, args) = parser.parse_args()
 tgtFile = options.targetpage
@@ -36,17 +36,20 @@ if (tgtFile == None):
 
 #### Creates KML header
 z = open('cellTowers.kml', 'w')
-z.write("<?xml version='1.0' encoding='UTF-8'?>\n")
-z.write("<kml xmlns='http://www.opengis.net/kml/2.2'>\n" )
-z.write("<Document>\n")
-z.write("<name>Cell Towers from Anaximander</name>\n")
-z.write("<description>Automatically generated using Anaximander (https://github.com/azmatt/Anaximander)</description>\n")
+z.write('<?xml version="1.0" encoding="UTF-8"?>\n')
+z.write('<kml xmlns="http://www.opengis.net/kml/2.2">\n')
+z.write('<Document>\n')
+z.write('<name>Cell Towers from Anaximander</name>\n')
+z.write('<description>Automatically generated using Anaximander (https://github.com/azmatt/Anaximander)</description>\n')
 # Use Pink Paddle icons for the points so they stand out in Google Earth
-z.write("   <Style id='pinkPaddle'><IconStyle><Icon><href>http://maps.google.com/mapfiles/kml/paddle/pink-blank.png</href></Icon></IconStyle></Style>\n\n")
+z.write('   <Style id="pinkPaddle"><IconStyle><Icon><href>http://maps.google.com/mapfiles/kml/paddle/pink-blank.png</href></Icon></IconStyle></Style>\n\n')
 
 #### Open XML document using minidom parser
 DOMTree = minidom.parse(tgtFile)
 models = DOMTree.getElementsByTagName('model')
+varNumModels = len(models)
+print('[+] There are {} records to extract.'.format(varNumModels))
+varCounter = 1
 
 #### Open SQLite Database with Cell Tower Location Info
 db = sqlite3.connect('cellTowers.sqlite')
@@ -74,21 +77,22 @@ for model in models:
             varLac = nameDatasplit[3].split()[1]
             varCid = nameDatasplit[4].split()[1]
             #print(varCellTowerType, varMcc, varMnc, varLac, varCid) #DEBUG
-    print("[+] Extracted record (MCC: %s, MNC: %s, LAC: %s, CID: %s)" % (varMcc, varMnc, varLac, varCid))
+    print('[+] Extracted record (MCC: {}, MNC: {}, LAC: {}, CID: {}) - Rcd# {} / {}'.format(varMcc, varMnc, varLac, varCid, varCounter, varNumModels))
 
     ### Search DB for Cell Tower Locations
-    print("[+]    Searching DB for this record.")
-    cursor.execute("select * from towers where mcc = '%s' and net = '%s' and area = '%s' and cell = '%s' limit 1" % (varMcc, varMnc, varLac, varCid))
+    print('[+]    Searching DB for this record.')
+    cursor.execute("select * from towers where mcc = '{}' and net = '{}' and area = '{}' and cell = '{}' limit 1".format(varMcc, varMnc, varLac, varCid))
     for row in cursor:
         try:
             varLon = row[6]
             varLat = row[7]
-            print("[+]    Found: (Lon: %s, Lat: %s), Writing to KML." % (varLon, varLat))
+            print('[+]    Found: (Lon: {}, Lat: {}), Writing to KML.'.format(varLon, varLat))
             kml_contents = "<Placemark>\n<TimeStamp><when>"+ varTimestamp +"</when></TimeStamp><name>" + str(varTimestamp)+ "</name>\n<description><p><b>Cell ID:</b> " + varCid + "</p><p><b>Date:</b> " + varTimestamp + "</p></description>\n<Point>\n<coordinates>" + varLon + "," + varLat + ",0</coordinates>\n</Point>\n<styleUrl>#pinkPaddle</styleUrl></Placemark>\n"
             z.write(kml_contents)
         except Exception as e:
-            print("\n[!!!]    Error (%s) with the following record (MCC: %s, MNC: %s, LAC: %s, CID: %s)\n" % (e, varMcc, varMnc, varLac, varCid))
+            print('\n[!!!]    Error ({}) with the following record (MCC: {}, MNC: {}, LAC: {}, CID: {})\n'.format(e, varMcc, varMnc, varLac, varCid))
+    varCounter += 1
 cursor.close
 
 closeKml()
-print("[+] Script completed with %s Records Parsed" % len(models))
+print('[+] Script completed.')
